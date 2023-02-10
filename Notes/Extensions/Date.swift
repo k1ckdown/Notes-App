100fdb97453394818de765e7111ceb7612b20a91
Ivan <Tomsk16121@yandex.ru>
2023-02-11 00:56:11 +0700

    extensions date

---
+++ Notes/Extensions/Date.swift
@@ -0,0 +1,23 @@
+//
+//  Date.swift
+//  Notes
+//
+//  Created by Ivan Semenov on 11.02.2023.
+//
+
+import Foundation
+
+extension Date {
+    func format() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        
+        if Calendar.current.isDateInToday(self) {
+            dateFormatter.dateFormat = "h:mm a"
+        } else {
+            dateFormatter.dateFormat = "MMM d, yyyy"
+        }
+        
+        return dateFormatter.string(from: self)
+    }
+}
